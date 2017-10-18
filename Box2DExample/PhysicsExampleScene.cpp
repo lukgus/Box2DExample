e1f578740fb8bc407b5ad2632ba1970e68504993
sakul_ca <sakul_ca@yahoo.com>
2017-10-17 21:13:10 -0400

    Added quick Scene class.

---
+++ Box2DExample/PhysicsExampleScene.cpp
@@ -0,0 +1,46 @@
+#include "PhysicsExampleScene.h"
+
+/**
+ * Default constructor
+ */
+PhysicsExampleScene::PhysicsExampleScene()
+{
+}
+
+/**
+ * Destructor
+ */
+PhysicsExampleScene::~PhysicsExampleScene()
+{
+
+}
+
+/**
+ * Initialize
+ * Called to initialize the scene
+ * @return bool - TRUE if successful, FALSE otherwise
+ */
+bool PhysicsExampleScene::Initialize()
+{
+	return true;
+}
+
+/**
+ * Destroy
+ * Destroys the scene, deletes everything, and sets up
+ * for closing.
+ */
+void PhysicsExampleScene::Destroy()
+{
+}
+
+/**
+ * UPdate
+ * Calls update on everything that needs to be updated
+ * at the scene level.
+ * @param dt - the delta time in ms
+ */
+void PhysicsExampleScene::Update(float dt)
+{
+
+}
---
+++ Box2DExample/PhysicsExampleScene.h
@@ -0,0 +1,19 @@
+#ifndef PHYSICS_EXAMPLE_SCENE_HG
+#define PHYSICS_EXAMPLE_SCENE_HG
+
+class PhysicsExampleScene
+{
+public:
+	PhysicsExampleScene();
+	~PhysicsExampleScene();
+
+	bool Initialize();
+	void Destroy();
+
+	void Update(float dt);
+
+private:
+
+};
+
+#endif
